e332ae966776d4fb070131c4ca4856c8d3d2ea8e
Luo Reid <luo.reid@gmail.com>
2019-03-11 21:16:59 +0800

    router active class

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="menu">
-    <router-link v-bind:to="{ name:'home'}">Home</router-link>
-    <router-link v-bind:to="{ name:'faq'}">FAQ</router-link>
+    <router-link v-bind:to="{ name:'home'}" exact>Home</router-link>
+    <router-link v-bind:to="{ name:'faq'}" exact>FAQ</router-link>
   </nav>
 </template>
 <script>
@@ -11,3 +11,10 @@ export default {
 };
 </script>
 
+<style lang="stylus" scoped>
+@import '../style/imports';
+
+.router-link-active {
+  border-bottom-color: $primary-color;
+}
+</style>
