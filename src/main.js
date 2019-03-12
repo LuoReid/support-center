0fa8dda051fb4009da85f97a707d5385d9716d5f
Luo Reid <luo.reid@gmail.com>
2019-03-12 22:32:59 +0800

    plugins fetch

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import Vue from 'vue'
 import AppLayout from './components/AppLayout.vue'
 import router from './router'
 import './global-components'
+import VueFetch from './plugins/fetch'
 
+Vue.use(VueFetch, { baseUrl: 'http://127.0.0.1:3000/' })
 new Vue({ el: '#app', render: h => h(AppLayout), router, })
