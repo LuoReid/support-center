1e2eb28712553c40e230ab4ec366bd582acd1d6e
Luo Reid <luo.reid@gmail.com>
2019-03-15 22:25:38 +0800

    user menu

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -2,12 +2,26 @@
   <nav class="menu">
     <router-link v-bind:to="{ name:'home'}" exact>Home</router-link>
     <router-link v-bind:to="{ name:'faq'}" exact>FAQ</router-link>
+    <div class="spacer"></div>
+    <template v-if="$state.user">
+      <a>{{ $state.user.username}}</a>
+      <a v-on:click="logout">Logout</a>
+    </template>
+    <router-link v-else v-bind:to="{name:'login'}">Login</router-link>
   </nav>
 </template>
 <script>
 import NavMenu from "./NavMenu.vue";
 export default {
-  components: { NavMenu }
+  components: { NavMenu },
+  methods:{
+    async logout(){
+      const result = await this.$fetch('logout')
+      if(result.status === 'ok'){
+        this.$state.user = null
+      }
+    }
+  }
 };
 </script>
 
