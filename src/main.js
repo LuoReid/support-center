9f7488922cba16c36aa5e2c87311c6ea750c8c75
Luo Reid <luo.reid@gmail.com>
2019-03-16 14:44:46 +0800

    init user

--- src/main.js
+++ src/main.js
@@ -3,14 +3,25 @@ import Vue from 'vue'
 import AppLayout from './components/AppLayout.vue'
 import router from './router'
 import './global-components'
-import VueFetch from './plugins/fetch'
+import VueFetch, { $fetch } from './plugins/fetch'
 import state from './state'
 import VueState from './plugins/state'
-Vue.use(VueState,state)
+
+Vue.use(VueState, state)
 Vue.use(VueFetch, { baseUrl: 'http://127.0.0.1:3000/' })
-new Vue({
+
+
+async function main() {
+  try {
+    state.user = await $fetch('user')
+  } catch (error) {
+    console.warn(e)
+  }
+  new Vue({
     el: '#app',
     data: state,
     router,
     render: h => h(AppLayout),
-})
+  })
+}
+main()
