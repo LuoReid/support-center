586d5716cf140bb7c5031591c792fab3386f5987
Luo Reid <luo.reid@gmail.com>
2019-03-16 14:48:25 +0800

    guest router

--- src/router.js
+++ src/router.js
@@ -12,17 +12,19 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', component: Home, name: 'home' },
   { path: '/faq', component: FAQ, name: 'faq' },
-  { path: '/login', name: 'login', component: Login },
+  { path: '/login', name: 'login', component: Login, meta: { guest: true } },
   { path: '/tickets', name: 'tickets', component: TicketsLayout, meta: { private: true } },
 ]
 
 const router = new VueRouter({ routes, mode: 'history', })
 
 router.beforeEach((to, from, next) => {
-  console.log('to', to.name)
   if (to.meta.private && !state.user) {
-    //TODO login
     next({ name: 'login', params: { wantedRoute: to.fullPath, } })
+    return
+  }
+  if (to.meta.guest && state.user) {
+    next({ name: 'home' })
     return
   }
   next()
